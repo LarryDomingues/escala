04bb5105a931424f2e2469b4980c90d4bab6bddb
zzeis <moisesmonteiro.stark@gmail.com>
2026-07-23 14:47:32 -0300

    Fix formatting in _document.js by removing unnecessary whitespace

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,9 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
-     
+      
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         {/* Mover a fonte para cá */}
